833e885c3aa581f991a47e815a3d8c9b9fd675c6
andinet2019 <andinetmn@gmail.com>
2021-03-14 22:13:25 -0500

    message

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Header() {
   return (
     <grid>
-      <h1 className="header">Puppies for Sale</h1>
+      <h1 style={{ alignItems: "center" }} className="header">
+        Puppies for Sale
+      </h1>
     </grid>
   );
 }
